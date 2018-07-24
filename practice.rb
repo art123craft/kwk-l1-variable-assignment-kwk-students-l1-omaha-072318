e78b05b110f200d423ebec136ef7ef32f4a0b6ec
art123craft <cammiebluecammie@gmail.com>
2018-07-24 19:16:31 +0000

    automatically backed up by learn

--- practice.rb
+++ practice.rb
@@ -2,3 +2,17 @@ def say_hello_one
   your_name = "Charlie"
   puts "Hello #{your_name}!"
 end
+
+def say_hello_two
+  your_name = "Kim"
+  puts "Hello #{your_name}!"
+end
+
+def say_hello_three
+  your_name = "John"
+  puts "Hello #{your_name}!"
+end
+
+say_hello_one
+say_hello_two
+say_hello_three
